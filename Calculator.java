a630c4c9d82e2649c23baf1df1a1ada2c02152fb
arundhatiharnoorkar <arundhatiharnoorkar@gmail.com>
2026-04-22 22:31:38 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,6 @@
 public class Calculator {
 
-    public int add(int a, int b) {
-        return a + b;
-    }
+    
 
     public int subtract(int a, int b) {
         return a - b;
@@ -22,7 +20,7 @@ public class Calculator {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
 
-        System.out.println(calc.add(10, 5));
+        
         System.out.println(calc.subtract(10, 5));
         System.out.println(calc.multiply(10, 5));
         System.out.println(calc.divide(10, 5));
