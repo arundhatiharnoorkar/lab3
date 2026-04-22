4c968b618975e6f024ee2deebdf802f1c7c15a31
arundhatiharnoorkar <arundhatiharnoorkar@gmail.com>
2026-04-22 22:16:41 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -16,8 +16,7 @@ public class Calculator {
 
     public int divide(int a, int b) {
         if (b == 0) {
-            throw new ArithmeticException("Division by zero is not 
-allowed.");
+            throw new ArithmeticException("Division by zero is not allowed.");
         }
         return a / b;
     }
